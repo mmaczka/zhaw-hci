b65c1f35818306f9606882744fd13e111db394be
michal <michal.maczka@hubvetia.com>
2015-04-28 10:02:06 +0200

    intermedaite state

---
+++ app/server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://192.168.59.103/hci-dev'
+  },
+
+  seedDB: true
+};
